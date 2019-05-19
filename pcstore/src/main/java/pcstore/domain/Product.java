6cdf62d1eff5d2c6b998ac1810b6ea8370f73dbd
Michal Matt <michu.em@gmail.com>
2019-05-19 14:59:39 +0200

    almost empty project for PC store componenst REST API: Product and Comment classes plus TestRest

---
+++ pcstore/src/main/java/pcstore/domain/Product.java
@@ -0,0 +1,44 @@
+package pcstore.domain;
+
+import java.util.List;
+
+public class Product {
+	
+	private int id;
+	private String name;
+	private double price;
+	private String category;
+	private List<Comment> comments;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+}
